32c5da98844dea40e60d6245124c78e3d333ee3d
christopherdent <christopherdent01@gmail.com>
2021-08-22 20:31:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Welcome to Battle, Ships 2</code>
         </p>
         <a
           className="App-link"
