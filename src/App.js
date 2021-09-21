9fc9b4901986e8df4d35e2d4afc83281591d58b3
Christopher Dent <christopherdent01@gmail.com>
2021-09-20 21:16:30 -0400

    add ships to app state

--- src/App.js
+++ src/App.js
@@ -3,29 +3,39 @@ import React from 'react';
 import './App.css';
 import SplitPane from 'react-split-pane';
 import Pane from 'react-split-pane';
-
+import ShipCard from './components/ShipCard'
 
 class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      ships: null
+    };
+  }
 
 componentDidMount(){
+  this.fetchShips()
+}
+
+fetchShips(){
   return fetch("https://safe-gorge-11585.herokuapp.com/ships")
   .then(function(response) {
     return response.json();
-  })
-  .then(json => {
-   json.forEach(ship => console.log(ship.name));
-    let arr = []
-    json.forEach(ship => arr.push(ship))
-    this.shuffle(arr)
-    // arr.forEach(ship => ShipCard.build(ship))
-     
-    // spinner.setAttribute('hidden', "");
-    // avail.removeAttribute('hidden')
-   })
+    })
+  .then(data => {
+
+   this.setState({
+    isLoaded: true,
+    ships: data
+    })
+    console.log(this.state)
+  })  
   .catch(function(error) {
-    console.log(error.message);
-  });
+    console.log(error.message)
+  })
+
+  
 }
 
   render() {
@@ -40,6 +50,7 @@ componentDidMount(){
         </Pane>
         <Pane initialSize="500px">
           <div class='top'>This is a pane in a fragment
+          <ShipCard />
 
           </div>
         </Pane>
--- src/components/ShipCard.js
+++ src/components/ShipCard.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
 
 const ShipCard = (props) => {
-
+console.log(props)
   return (
 
-
+    
       <Card
         variant = 'top'
         key={props.id}
@@ -15,7 +14,7 @@ const ShipCard = (props) => {
         className="mb-2"
         >
       
-        <Card.Img src={props.pictureUrl} alt='prehistoric world' />
+        <Card.Img src={props.pictureUrl} alt='Ship' />
 
           <Card.Title>
         <h4 className = 'text-center'>{props.name}</h4>
